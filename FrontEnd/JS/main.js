b84a16d7e29c3cadc91667e603b7dcef5ffba7ef
Davina Crahet <davina.crahet@gmail.com>
2024-01-07 23:39:29 +0100

    Correction des erreurs W3C

--- FrontEnd/JS/main.js
+++ FrontEnd/JS/main.js
@@ -37,6 +37,7 @@ GetCategories().then(() => {
 async function getWorks() {
   const reponse = await fetch("http://localhost:5678/api/works");
   const works = await reponse.json();
+  console.log(works);
   return works;
 }
 
